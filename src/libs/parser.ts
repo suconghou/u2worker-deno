fc4a51810143896e70acf0e84554886b4cb961a3
suconghou <1126045770@qq.com>
2021-10-11 23:42:51 +0800

    fix: update version

--- src/libs/parser.ts
+++ src/libs/parser.ts
@@ -31,10 +31,8 @@ class infoParser {
             "videoId": this.vid,
             "context": {
                 "client": {
-                    "hl": "en",
-                    "gl": "US",
                     "clientName": "ANDROID",
-                    "clientVersion": "16.02"
+                    "clientVersion": "16.13.35"
                 }
             }
         }
@@ -98,7 +96,7 @@ class infoParser {
             info['error'] = this.error
             return info
         }
-        for (let item of (this.streamingData.formats || [])) {
+        for (let item of (this.streamingData.formats || []).concat(this.streamingData.adaptiveFormats || [])) {
             const itag = String(item.itag)
             const s = {
                 "quality": item.qualityLabel || item.quality,
@@ -107,20 +105,11 @@ class infoParser {
                 "len": item.contentLength,
                 'url': await this.buildURL(item)
             } as any
-            streams[itag] = s
-        }
-        for (let item of (this.streamingData.adaptiveFormats) || []) {
-            const itag = String(item.itag)
-            const s = {
-                "quality": item.qualityLabel || item.quality,
-                "type": item.mimeType.replace(/\+/g, ' '),
-                "itag": itag,
-                "len": item.contentLength,
-                "initRange": item.initRange,
-                "indexRange": item.indexRange,
-                'url': await this.buildURL(item)
-            } as any
-            streams[itag] = s
+            if (item.initRange && item.indexRange) {
+                s["initRange"] = item.initRange;
+                s["indexRange"] = item.indexRange;
+            }
+            streams[itag] = s;
         }
         return info;
     }
